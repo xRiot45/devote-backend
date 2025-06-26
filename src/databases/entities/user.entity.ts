16c502a0ffcd30ae32c4be9adcc702739b5d0d83
rumahkodingku <rumahkodingku45@gmail.com>
2025-06-26 22:07:09 +0700

    feat: setup entity user

---
+++ src/databases/entities/user.entity.ts
@@ -0,0 +1,53 @@
+import { RoleEnum } from 'src/enums/role.enum';
+import { Column, CreateDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn } from 'typeorm';
+
+@Entity('users')
+export class User {
+    @PrimaryGeneratedColumn('increment')
+    id: number;
+
+    @Column({
+        type: 'varchar',
+        length: 255,
+        nullable: false,
+    })
+    walletAddress: string;
+
+    @Column({
+        type: 'varchar',
+        length: 100,
+        nullable: false,
+    })
+    name: string;
+
+    @Column({
+        type: 'varchar',
+        length: 100,
+        nullable: false,
+    })
+    email: string;
+
+    @Column({
+        type: 'enum',
+        enum: RoleEnum,
+        default: RoleEnum.USER,
+    })
+    role: string | RoleEnum;
+
+    @CreateDateColumn({
+        type: 'timestamp',
+        default: () => 'CURRENT_TIMESTAMP(6)',
+    })
+    createdAt: Date;
+
+    @UpdateDateColumn({
+        type: 'timestamp',
+        default: () => 'CURRENT_TIMESTAMP(6)',
+        onUpdate: 'CURRENT_TIMESTAMP(6)',
+    })
+    updatedAt: Date;
+
+    constructor(partial: Partial<User>) {
+        Object.assign(this, partial);
+    }
+}
