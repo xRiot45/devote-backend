8b0c42148f373eabf86d9a13713243a3ffd5b00d
rumahkodingku <rumahkodingku45@gmail.com>
2025-07-01 20:43:03 +0700

    refactor: refactoring code

--- src/apps/auth/auth.controller.ts
+++ src/apps/auth/auth.controller.ts
@@ -1,5 +1,4 @@
 import { Body, Controller, Post, Res } from '@nestjs/common';
-import { ApiBody, ApiOperation, ApiResponse as SwaggerApiResponse } from '@nestjs/swagger';
 import { Response } from 'express';
 import { AuthService } from './auth.service';
 import { AuthDto } from './dto/auth.dto';
@@ -9,38 +8,18 @@ export class AuthController {
     constructor(private readonly authService: AuthService) {}
 
     @Post('/wallet')
-    @ApiOperation({ summary: 'Authenticate with wallet address' })
-    @ApiBody({ type: AuthDto })
-    @SwaggerApiResponse({
-        status: 200,
-        description: 'Successfully authenticated and received access & refresh token.',
-        schema: {
-            example: {
-                success: true,
-                message: 'Authentication successful',
-                data: {
-                    accessToken: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9...',
-                    refreshToken: 'd9f8s7d6f8sd7f6sdf...',
-                },
-            },
-        },
-    })
-    @SwaggerApiResponse({
-        status: 400,
-        description: 'Invalid wallet address or request data',
-    })
     public async authWithWalletAddress(
-        @Body() authDto: AuthDto,
-        @Res({ passthrough: true }) res: Response,
+        @Body() body: AuthDto,
+        @Res({ passthrough: true }) response: Response,
     ): Promise<ApiResponse<{ accessToken: string; refreshToken: string }>> {
-        const { data, message } = await this.authService.authWithWalletAddress(authDto);
-        const { accessToken, refreshToken } = data;
+        const { data, message } = await this.authService.authWithWalletAddress(body);
+        const { accessToken, refreshToken } = data as { accessToken: string; refreshToken: string };
 
-        res.cookie('refreshToken', refreshToken, {
+        response.cookie('refreshToken', refreshToken, {
             httpOnly: true,
             secure: process.env.APP_ENV === 'production',
             sameSite: 'strict',
-            maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
+            maxAge: 7 * 24 * 60 * 60 * 1000,
         });
 
         return {
