bff231f3a3c35c10570cb475cfdd5163e8e80892
rumahkodingku <rumahkodingku45@gmail.com>
2025-07-08 16:20:52 +0700

    refactor: refactoring code

--- src/apps/voting-sessions/dto/update-voting-session.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/swagger';
-import { CreateVotingSessionDto } from './create-voting-session.dto';
-
-export class UpdateVotingSessionDto extends PartialType(CreateVotingSessionDto) {}
